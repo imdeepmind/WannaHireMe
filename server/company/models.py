5148a9f0b3ca9cf43a957cbd727c5116d82f0dcf
Abhishek Chatterjee <abhishekchatterjeejit@gmail.com>
2025-02-06 23:59:07 +0530

    Added models for Company

--- server/company/models.py
+++ server/company/models.py
@@ -1,3 +1,24 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from core.models import LinkType
+
+
+class Company(models.Model):
+    TIER_CHOICES = [("tier_1", "Tier 1"), ("tier_2", "Tier 2"), ("tier_3", "Tier 3")]
+
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(blank=True, null=True)
+    logo_uri = models.CharField(max_length=255)
+    tier = models.CharField(max_length=30, choices=TIER_CHOICES)
+
+    def __str__(self):
+        return f"Company {self.name}"
+
+
+class CompanyLink(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="Company")
+    link_type = models.ForeignKey(LinkType, on_delete=models.CASCADE, related_name="LinkTypeCompany")
+    url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.url
