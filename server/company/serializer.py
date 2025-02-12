c85fa477039f05dbee1c8312b5fbfb4afbdd63e5
Abhishek Chatterjee <abhishek@techvariable.com>
2025-02-12 13:55:44 +0530

    Added Create and List API

--- server/company/serializer.py
+++ server/company/serializer.py
@@ -16,9 +16,12 @@ class CompanyLinkSerializer(ModelSerializer):
 
 
 class CompanySkillSerializer(ModelSerializer):
+    skill = PrimaryKeyRelatedField(queryset=SkillModel.objects.all(), write_only=True)
+    skill_details = SkillSerializer(source="skill", read_only=True)
+
     class Meta:
         model = CompanySkillModel
-        fields = ["company", "name"]
+        fields = ["skill", "skill_details"]
 
 
 class CompanySerializer(ModelSerializer):
@@ -27,6 +30,9 @@ class CompanySerializer(ModelSerializer):
 
     country_details = CountrySerializer(source="country", read_only=True)
     state_details = StateSerializer(source="state", read_only=True)
+
+    links = CompanyLinkSerializer(many=True, required=False, source="company_links_for_link")
+    skills = CompanySkillSerializer(many=True, required=False, source="company_skill_company")
 
     class Meta:
         model = CompanyModel
@@ -40,4 +46,47 @@ class CompanySerializer(ModelSerializer):
             "country_details",
             "state",
             "state_details",
+            "links",
+            "skills",
         ]
+
+    def validate_links(self, links):
+        if len(links) > 5:
+            raise ValidationError("Only 5 links can be added")
+
+        return links
+
+    def validate_skills(self, skills):
+        if len(skills) > 10:
+            raise ValidationError("Only 10 skills can be added")
+
+        return skills
+
+    def create(self, validated_data):
+        links_data = validated_data.pop("company_links_for_link", [])
+        skills_data = validated_data.pop("company_skill_company", [])
+
+        if len(links_data) > 5:
+            raise ValidationError("Only 5 links can be added")
+
+        if len(skills_data) > 10:
+            raise ValidationError("Only 10 skills can be added")
+
+        country = validated_data.pop("country")
+        state = validated_data.pop("state")
+
+        instance = CompanyModel.objects.create(country=country, state=state, **validated_data)
+
+        CompanyLinkModel.objects.bulk_create(
+            [
+                CompanyLinkModel(
+                    company=instance,
+                    **link,
+                )
+                for link in links_data
+            ]
+        )
+
+        CompanySkillModel.objects.bulk_create([CompanySkillModel(company=instance, **skill) for skill in skills_data])
+
+        return instance
