91309258acb9f0102b54c57807becb6a173baf64
Abhishek Chatterjee <abhishek@techvariable.com>
2025-02-12 15:28:47 +0530

    Added SearchCompany API

--- server/company/urls.py
+++ server/company/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from .views import ListOrCreateCompany, RetrieveOrUpdateOrDeleteCompany
+from .views import ListOrCreateCompany, RetrieveOrUpdateOrDeleteCompany, SearchCompany
+
+router = DefaultRouter()
+router.register(r'companies', SearchCompany, basename='company')
 
 urlpatterns = [
     path("", ListOrCreateCompany.as_view(), name="Create or List Companies"),
-    path("<int:pk>", RetrieveOrUpdateOrDeleteCompany.as_view(), name="Retrieve or Update or Delete Company")
+    path("<int:pk>", RetrieveOrUpdateOrDeleteCompany.as_view(), name="Retrieve or Update or Delete Company"),
+    path("search/", include(router.urls))
 ]
--- server/company/views.py
+++ server/company/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Q
+
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Company as CompanyModel
 from .serializer import CompanySerializer
@@ -10,6 +13,31 @@ class ListOrCreateCompany(ListCreateAPIView):
     pagination_class = PageNumberPagination
     serializer_class = CompanySerializer
 
+
 class RetrieveOrUpdateOrDeleteCompany(RetrieveUpdateDestroyAPIView):
     queryset = CompanyModel.objects.all()
     serializer_class = CompanySerializer
+
+
+class SearchCompany(ReadOnlyModelViewSet):
+    serializer_class = CompanySerializer
+
+    def get_queryset(self):
+        queryset = CompanyModel.objects.all()
+        request = self.request
+        params = request.GET
+
+        query = params.get("query")
+        sortField = params.get('sort')
+        sortOrder = params.get('order')
+
+        if query:
+            queryset = queryset.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+        if sortField:
+            if sortOrder == "desc":
+                queryset = queryset.order_by(f"-{sortField}")
+            else:
+                queryset = queryset.order_by(f"{sortField}")
+
+        return queryset
